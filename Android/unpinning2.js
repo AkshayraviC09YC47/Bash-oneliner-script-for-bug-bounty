1aa4496eaf787c469d89a41d2bb61e15d3987b51
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2023-03-22 12:23:04 +0530

    Update unpinning2.js

--- Android/unpinning2.js
+++ Android/unpinning2.js
@@ -3,9 +3,9 @@
 
 Java.perform(function () {
 	console.log('')
-	console.log('===')
+	console.log('===========================================================')
 	console.log('* Injecting hooks into common certificate pinning methods *')
-	console.log('===')
+	console.log('===========================================================')
 
 	var X509TrustManager = Java.use('javax.net.ssl.X509TrustManager');
 	var SSLContext = Java.use('javax.net.ssl.SSLContext');
@@ -31,66 +31,66 @@ Java.perform(function () {
 		'[Ljavax.net.ssl.KeyManager;', '[Ljavax.net.ssl.TrustManager;', 'java.security.SecureRandom'
 	);
 	SSLContext_init.implementation = function (keyManager, trustManager, secureRandom) {
-		console.log('! Intercepted trustmanager request');
+		console.log('[+] Intercepted trustmanager request');
 		SSLContext_init.call(this, keyManager, TrustManagers, secureRandom);
 	};
 
-	console.log('* Setup custom trust manager');
+	console.log('[*] Setup custom trust manager');
 
 	// okhttp3
 	try {
 		var CertificatePinner = Java.use('okhttp3.CertificatePinner');
 		CertificatePinner.check.overload('java.lang.String', 'java.util.List').implementation = function (str) {
-			console.log('! Intercepted okhttp3: ' + str);
+			console.log('[+] Intercepted okhttp3: ' + str);
 			return;
 		};
 
-		console.log('* Setup okhttp3 pinning')
+		console.log('[+] Setup okhttp3 pinning')
 	} catch(err) {
-		console.log('* Unable to hook into okhttp3 pinner')
+		console.log('[!] Unable to hook into okhttp3 pinner')
 	}
 
 	// trustkit
 	try {
 		var Activity = Java.use("com.datatheorem.android.trustkit.pinning.OkHostnameVerifier");
 		Activity.verify.overload('java.lang.String', 'javax.net.ssl.SSLSession').implementation = function (str) {
-			console.log('! Intercepted trustkit{1}: ' + str);
+			console.log('[+] Intercepted trustkit{1}: ' + str);
 			return true;
 		};
 
 		Activity.verify.overload('java.lang.String', 'java.security.cert.X509Certificate').implementation = function (str) {
-			console.log('! Intercepted trustkit{2}: ' + str);
+			console.log('[+] Intercepted trustkit{2}: ' + str);
 			return true;
 		};
 
-		console.log('* Setup trustkit pinning')
+		console.log('[*] Setup trustkit pinning')
 	} catch(err) {
-		console.log('* Unable to hook into trustkit pinner')
+		console.log('[!] Unable to hook into trustkit pinner')
 	}
 
 	// TrustManagerImpl
 	try {
 		var TrustManagerImpl = Java.use('com.android.org.conscrypt.TrustManagerImpl');
 		TrustManagerImpl.verifyChain.implementation = function (untrustedChain, trustAnchorChain, host, clientAuth, ocspData, tlsSctData) {
-			console.log('! Intercepted TrustManagerImp: ' + host);
+			console.log('[+] Intercepted TrustManagerImp: ' + host);
 			return untrustedChain;
 		}
 
-		console.log('* Setup TrustManagerImpl pinning')
+		console.log('[+] Setup TrustManagerImpl pinning')
 	} catch (err) {
-		console.log('* Unable to hook into TrustManagerImpl')
+		console.log('[!] Unable to hook into TrustManagerImpl')
 	}
 
 	// Appcelerator
 	try {
 		var PinningTrustManager = Java.use('appcelerator.https.PinningTrustManager');
 		PinningTrustManager.checkServerTrusted.implementation = function () {
-			console.log('! Intercepted Appcelerator');
+			console.log('[+] Intercepted Appcelerator');
 		}
 
-		console.log('* Setup Appcelerator pinning')
+		console.log('[+] Setup Appcelerator pinning')
 	} catch (err) {
-		console.log('* Unable to hook into Appcelerator pinning')
+		console.log('[!] Unable to hook into Appcelerator pinning')
 	}
 	
 	// ByPass SSL pinning for Android 7+

329446f26b17a2f699ffec8c5d492aac6368aa37
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2025-01-02 11:36:22 +0530

    Create global_keyboard_interrupt_handler.py

---
+++ misc/global_keyboard_interrupt_handler.py
@@ -0,0 +1,15 @@
+import functools
+
+def global_keyboard_interrupt_handler(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyboardInterrupt:
+            print("\nQuitting...")
+            exit(1)
+    return wrapper
+
+@global_keyboard_interrupt_handler
+name = input("[+]Name: ")
+print(name)
